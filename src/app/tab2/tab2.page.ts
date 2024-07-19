e26ba5427a1894ea2f5f1089a103525e1e21d81e
Marlenne <Marlenneibarr@gmail.com>
2024-07-19 14:05:52 -0600

    v2

---
+++ src/app/tab2/tab2.page.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { PhotoService } from '../services/photo.service';
+
+@Component({
+  selector: 'app-tab2',
+  templateUrl: 'tab2.page.html',
+  styleUrls: ['tab2.page.scss']
+})
+export class Tab2Page {
+  photos:any[]=[];
+
+  constructor(public photoService: PhotoService) {
+    this.photos=this.photoService.photos
+  }
+
+  takePhoto() {
+    this.photoService.addPhoto();
+  }
+
+}
---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.log(err));
+
+  import { defineCustomElements } from '@ionic/pwa-elements/loader';
+import { environment } from './environments/environment.prod';
+import { enableProdMode } from '@angular/core';
+
+// Call the element loader before the bootstrapModule/bootstrapApplication call
+defineCustomElements(window);
+if (environment.production) {
+  enableProdMode();
+}
